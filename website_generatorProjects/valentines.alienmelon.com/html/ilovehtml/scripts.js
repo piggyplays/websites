43e3468f3b4cfa50589620ad11e13b75dda94bf3
piggyplays <84114168+piggyplays@users.noreply.github.com>
2022-05-16 20:15:02 -0700

    Delete website_generatorProjects directory

--- website_generatorProjects/valentines.alienmelon.com/html/ilovehtml/scripts.js
+++
@@ -1,7 +0,0 @@
-function save_canvas(){
-	var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"); 
-    $('.save').attr({
-    'download':'test.png',
-    'href':image
-     });
-}
